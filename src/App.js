e00c77e4a7f5756d6bb1dc7a3c827740e5e02852
matheusfosilva <fermath99@gmail.com>
2022-01-06 00:45:43 -0300

    Add search, config details and maps

--- src/App.js
+++ src/App.js
@@ -1,66 +1,245 @@
 import './App.css';
-import {Form,Container,Card, Button } from 'react-bootstrap';
+import { Form, Container, Card, Button, Modal, Table, Figure, Alert } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import MapPicker from 'react-google-map-picker';
 
+const DefaultLocation = { lat: -7.220086, lng: -39.3281503 };
 
 function App() {
 
   //const [input, setInput ] = useState(0);
-  const [comics, setComics ] = useState([]);
-    
-    async function getComics(){
+  const [comics, setComics] = useState([]);
+  const [visible, setVisible] = useState(false);
+  const [comicModal, setComicModal] = useState(null);
+  const [search, setSearch] = useState('');
+  const [selectComics, setSelectComics] = useState([]);
+  const [visibleMap, setVisibleMap] = useState(false);
+  const [zoom, setZoom] = useState(10);
+  const [defaultLocation, setDefaultLocation] = useState(DefaultLocation);
+  const [location, setLocation] = useState(defaultLocation)
+  const [sendMessage, setSendMessage] = useState(false)
 
-        const timeStamp = '1641319856';
-        const apiKey = '2690be4165b30b415ca788849e00c3dd';
-        const md5 = 'b94d9effe9952c56847c56eba20cb753';
+  const timeStamp = '1641319856';
+  const apiKey = '2690be4165b30b415ca788849e00c3dd';
+  const md5 = 'b94d9effe9952c56847c56eba20cb753';
 
-      try {
-        const response  = await axios.get(`http://gateway.marvel.com/v1/public/comics?ts=${timeStamp}&apikey=${apiKey}&hash=${md5}`);
-        setComics(response.data.data.results)
-        console.log(response.data.data.results);
-      } catch (error) {
-        console.error(error);
-      }
+  async function getComics() {
 
+    try {
+      const response = await axios.get(`http://gateway.marvel.com/v1/public/comics?ts=${timeStamp}&apikey=${apiKey}&hash=${md5}`);
+      setComics(response.data.data.results)
+      console.log(response.data.data.results);
+    } catch (error) {
+      console.error(error);
     }
 
-    useEffect(() => {
-      getComics()      
-    },[]);
+  }
+
+  async function searchComics() {
+
+    try {
+      const response = await axios.get(`http://gateway.marvel.com/v1/public/comics?ts=${timeStamp}&apikey=${apiKey}&hash=${md5}&title=${search}`);
+      setComics(response.data.data.results)
+      console.log(response.data.data.results);
+    } catch (error) {
+      console.error(error);
+    }
+
+  }
+
+  function toggleSendMessage() {
+    setSendMessage(!sendMessage);
+    setVisibleMap(false);
+  }
+
+  function handleChangeLocation(lat, lng) {
+    setLocation({ lat: lat, lng: lng });
+  }
+
+  function handleChangeZoom(newZoom) {
+    setZoom(newZoom);
+  }
+
+  function showModal(comic) {
+    setVisible(true);
+    setComicModal(comic);
+  }
+
+  function hideModal() {
+    setVisible(false);
+    setComicModal(null);
+  }
+
+  function showMap() {
+    setVisibleMap(true);
+  }
+
+  function hideMap() {
+    setVisibleMap(false);
+  }
+
+  useEffect(() => {
+    console.log(selectComics)
+  }, [selectComics]);
+
+  useEffect(() => {
+    getComics()
+  }, []);
+
+  useEffect(() => {
+    if (search?.length > 0) {
+      searchComics()
+    } else {
+      getComics();
+    }
+  }, [search]);
 
   return (
     <div className="App">
-      
-       <Container className='container'>
-          <Form>
-            <Form.Control type="text" placeholder="Digite o nome do quadrinho"  />
-          </Form>
-       </Container>
-      
+
+      <Container className='container'>
+        <Form>
+          <Form.Control value={search} type="text" placeholder="Search a comic" onChange={(text) => setSearch(text.target.value)} />
+        </Form>
+      </Container>
+
+      <Container>
+
+        {selectComics?.length > 0 && (
+          <div>
+
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>Imagem</th>
+                  <th>Nome do quadrinho</th>
+                </tr>
+              </thead>
+              <tbody>
+                {selectComics.map((comic) => {
+                  return (
+                    <tr>
+                      <td>
+                        <Figure>
+                          <Figure.Image
+                            width={50}
+                            src={comic.thumbnail.path + '.' + comic.thumbnail.extension}
+                          />
+                        </Figure>
+                      </td>
+                      <td>{comic.title}</td>
+                    </tr>
+                  )
+                })}
+
+              </tbody>
+            </Table>
+
+            <Button variant="primary" onClick={showMap}>Send-me</Button>
+
+            <Modal show={sendMessage}>
+              <Alert variant="success" style={{margin:'0px'}}>
+                <Alert.Heading style={{textAlign:'center'}}>Comics will send soon!</Alert.Heading>
+                <hr />
+                <div className="d-flex justify-content-end">
+                  <Button onClick={toggleSendMessage} variant="outline-success">
+                    Confirm
+                  </Button>
+                </div>
+              </Alert>
+            </Modal>
+
+            <Modal show={visibleMap}>
+              <Modal.Header >
+                <Modal.Title>Select your andress in the map: </Modal.Title>
+              </Modal.Header>
+
+              <Modal.Body>
+
+                <MapPicker defaultLocation={defaultLocation}
+                  zoom={zoom}
+                  mapTypeId="roadmap"
+                  style={{ height: '700px' }}
+                  onChangeLocation={handleChangeLocation}
+                  onChangeZoom={handleChangeZoom}
+                  apiKey='AIzaSyDaD-UyaZzx4ROA3VKDYQE9MyCXsDghkV8' />
+
+              </Modal.Body>
+
+              <Modal.Footer >
+                <Button variant="secondary" onClick={hideMap}>Back</Button>
+                <Button variant="primary" onClick={toggleSendMessage}>Send</Button>
+              </Modal.Footer>
+            </Modal>
+
+          </div>
+
+        )}
+      </Container >
+
+
       <Container style={
-       { display: 'flex', 
-        flexWrap: 'wrap',
-        justifyContent: 'center'
-      }
+
+        {
+          display: 'flex',
+          flexWrap: 'wrap',
+          justifyContent: 'center'
+        }
+
       }>
+
         {comics?.map((comic) => {
-            return ( 
-            <Card style={{  width: '18rem' , margin: '20px' ,  }}>
-            <Card.Img variant="top" src={comic.thumbnail.path + '.jpg'}  /> 
+          return (
+
+            <Card style={{ width: '18rem', margin: '20px', }}>
+              <Card.Img variant="top" src={comic.thumbnail.path + '.' + comic.thumbnail.extension} />
               <Card.Body>
-                <Card.Title>{comic.title}</Card.Title>
-                {/* <Card.Text>{comic.description}</Card.Text>  
-                <Button variant="primary">Detalhes </Button>  */}
+                <div style={{ display: 'flex', justifyContent: 'space-between', flexDirection: 'column' }}>
+                  <Card.Title style={{ flex: 1 }}>{comic.title}</Card.Title>
+                  <div style={{ display: 'flex', justifyContent: 'space-around' }} >
+                    <Button variant="outline-info" onClick={() => showModal(comic)}> Details </Button>
+
+                    {
+                      selectComics.find(element => element.id == comic.id) ?
+                        <Button variant="outline-danger" onClick={() => {
+                          setSelectComics(selectComics.filter(elemento => {
+                            return elemento.id != comic.id
+                          }))
+                        }} >Remove</Button>
+                        : <Button variant="outline-success" onClick={() => setSelectComics(selectComics.concat(comic))}>Add</Button>
+
+                    }
+                  </div>
+                </div>
               </Card.Body>
             </Card>
-        )
-          })}  
-      </Container>  
-      
-      
-    </div>
+
+          )
+        })}
+
+        {comicModal && (
+
+          <Modal show={visible}>
+            <Modal.Header >
+              <Modal.Title>{comicModal.title}</Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body>
+              <p>{comicModal.description?.length === 0 || comicModal.description?.length == null ? 'No description' : comicModal.description}</p>
+            </Modal.Body>
+
+            <Modal.Footer >
+              <Button variant="primary" onClick={hideModal} >Voltar</Button>
+            </Modal.Footer>
+          </Modal>
+        )}
+
+      </Container>
+
+
+    </div >
   );
 }
 
