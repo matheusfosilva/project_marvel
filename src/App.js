073043ed21ab63d9e7bec0a9f23de7f8d915e0f1
matheusfosilva <fermath99@gmail.com>
2022-01-07 12:13:59 -0300

    test e ajustes na table detail

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
-import { Form, Container, Card, Button, Modal, Table, Figure, Alert } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import MapPicker from 'react-google-map-picker';
+import { Form, Container, Card, Button, Modal, Table, Figure, Alert } from 'react-bootstrap'; //import de componentes do framework bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css'; //import de framework para auxiliar no design front-end
+import { useEffect, useState } from 'react'; 
+import axios from 'axios'; // import de ferramenta para consumo de api 
+import MapPicker from 'react-google-map-picker'; //import de biblioteca para exibição de mapa 
 
 const DefaultLocation = { lat: -7.220086, lng: -39.3281503 };
 
 function App() {
 
-  //const [input, setInput ] = useState(0);
+  // Definição de váriaveis para utilização nas modificações durante o uso da aplicação 
+
   const [comics, setComics] = useState([]);
   const [visible, setVisible] = useState(false);
   const [comicModal, setComicModal] = useState(null);
@@ -21,15 +22,28 @@ function App() {
   const [location, setLocation] = useState(defaultLocation)
   const [sendMessage, setSendMessage] = useState(false)
  
+  // Variáveis utilizadas para configurar string de request à api 
 
   const timeStamp = '1641319856';
   const apiKey = '2690be4165b30b415ca788849e00c3dd';
   const md5 = 'b94d9effe9952c56847c56eba20cb753';
 
-  async function getComics() {
+  async function getComics() { // função de request da api 
+
+    try { // função implementada para o tratamento de erros
+      const response = await axios.get(`http://gateway.marvel.com/v1/public/comics?ts=${timeStamp}&apikey=${apiKey}&hash=${md5}&limit=40`); // string de request da api 
+      setComics(response.data.data.results) // encapsulamento do response
+      console.log(response.data.data.results);
+    } catch (error) {
+      console.error(error);
+    }
+
+  }
+
+  async function searchComics() { // função de busca de quadrinhos semelhante à função de request com o parametro de tittle adicional na string de request
 
     try {
-      const response = await axios.get(`http://gateway.marvel.com/v1/public/comics?ts=${timeStamp}&apikey=${apiKey}&hash=${md5}&limit=40`);
+      const response = await axios.get(`http://gateway.marvel.com/v1/public/comics?ts=${timeStamp}&apikey=${apiKey}&hash=${md5}&title=${search}&limit=40`);
       setComics(response.data.data.results)
       console.log(response.data.data.results);
     } catch (error) {
@@ -38,58 +52,43 @@ function App() {
 
   }
 
-  async function searchComics() {
-
-    try {
-      const response = await axios.get(`http://gateway.marvel.com/v1/public/comics?ts=${timeStamp}&apikey=${apiKey}&hash=${md5}&title=${search}`);
-      setComics(response.data.data.results)
-      console.log(response.data.data.results);
-    } catch (error) {
-      console.error(error);
-    }
-
-  }
-
-  function toggleSendMessage() {
+  function toggleSendMessage() { //função de exibir/esconder modal de envio de quadrinhos
     setSendMessage(!sendMessage);
     setVisibleMap(false);
   }
 
-  function handleChangeLocation(lat, lng) {
+  function handleChangeLocation(lat, lng) {//função para configuração de local no mapa
     setLocation({ lat: lat, lng: lng });
   }
 
-  function handleChangeZoom(newZoom) {
+  function handleChangeZoom(newZoom) { //função de zoom do mapa pega da biblioteca 
     setZoom(newZoom);
   }
 
-  function showModal(comic) {
+  function showModal(comic) { //função de exibição de detalhes dos quadrinhos 
     setVisible(true);
     setComicModal(comic);
   }
 
-  function hideModal() {
+  function hideModal() { //função de inibir detalhes dos quadrinhos
     setVisible(false);
     setComicModal(null);
   }
 
-  function showMap() {
+  function showMap() { //função de exibição do mapa
     setVisibleMap(true);
   }
 
-  function hideMap() {
+  function hideMap() { //função de esconder o mapa 
     setVisibleMap(false);
   }
 
-  useEffect(() => {
+  useEffect(() => { // listener de atualização de página 
     getComics()
   }, []);
 
-  useEffect(() => {
-    getComics()
-  }, []);
-
-  useEffect(() => {
+  
+  useEffect(() => { //listener de atulização de página mediante procura de quadrinhos 
     if (search?.length > 0) {
       searchComics()
     } else {
@@ -103,7 +102,7 @@ function App() {
       <div style={{ display: 'flex', backgroundColor: 'black' }}>
         <Container className='container'>
           <Form>
-            <Form.Control data-testid="search" value={search} type="text" placeholder="Search a comic" onChange={(text) => setSearch(text.target.value)} />
+            <Form.Control data-testid="search" value={search} type="text" placeholder="Search a comic" onChange={(text) => setSearch(text.target.value)} /> {/*imput de busca de quadrinho */}
           </Form>
         </Container>
       </div>
@@ -111,14 +110,15 @@ function App() {
 
       <Container>
 
-        {selectComics?.length > 0 && (
-          <div>
+        {selectComics?.length > 0 && ( 
+          <div> 
 
-            <Table striped bordered hover>
+            <Table striped bordered hover> {/*lista de quadrinhos selecionados*/}
               <thead>
                 <tr>
                   <th>Image</th>
                   <th>Comic title</th>
+                  <th>Details</th>
                 </tr>
               </thead>
               <tbody>
@@ -134,6 +134,7 @@ function App() {
                         </Figure>
                       </td>
                       <td>{comic.title}</td>
+                      <td>{comic.description?.length < 5 || comic.description?.length == null ? 'No description' : comic.description}</td>
                     </tr>
                   )
                 })}
@@ -141,21 +142,21 @@ function App() {
               </tbody>
             </Table>
 
-            <Button variant="primary" onClick={showMap}>Send-me</Button>
+            <Button variant="primary" onClick={showMap}>Send-me</Button> {/*botão de envio com ação de exibir o mapa  */}
 
-            <Modal show={sendMessage} onHide={toggleSendMessage} >
+            <Modal show={sendMessage} onHide={toggleSendMessage} > {/*exibição do modal com mensgagem de envio de quadrinhos com função de escondê-lo caso haja algum click fora do modal */}
               <Alert variant="success" style={{ margin: '0px' }}>
                 <Alert.Heading style={{ textAlign: 'center' }} >Comics will send soon!</Alert.Heading>
                 <hr />
                 <div className="d-flex justify-content-end">
-                  <Button onClick={toggleSendMessage} variant="outline-success">
+                  <Button onClick={toggleSendMessage} variant="outline-success"> {/*botão com ação de esconder o modal de mensagem de envio */}
                     Confirm
                   </Button>
                 </div>
               </Alert>
             </Modal>
 
-            <Modal show={visibleMap} onHide={hideMap}>
+            <Modal show={visibleMap} onHide={hideMap}> {/* exibição do modal de mapa com ação de escondê-lo caso haja um click fora do modal  */}
               <Modal.Header >
                 <Modal.Title>Select your andress in the map: </Modal.Title>
               </Modal.Header>
@@ -168,12 +169,12 @@ function App() {
                   style={{ height: '700px' }}
                   onChangeLocation={handleChangeLocation}
                   onChangeZoom={handleChangeZoom}
-                  apiKey='AIzaSyDaD-UyaZzx4ROA3VKDYQE9MyCXsDghkV8' />
+                  apiKey='AIzaSyDaD-UyaZzx4ROA3VKDYQE9MyCXsDghkV8' /> {/*chave da api do google maps */}
 
               </Modal.Body>
 
-              <Modal.Footer >
-                <Button variant="secondary" onClick={hideMap}>Back</Button>
+              <Modal.Footer > {/*botões de envio de quadrinho e esconder modal*/}
+                <Button variant="secondary" onClick={hideMap}>Back</Button> 
                 <Button variant="primary" onClick={toggleSendMessage}>Send</Button>
               </Modal.Footer>
             </Modal>
@@ -194,10 +195,10 @@ function App() {
 
       }>
 
-        {comics?.map((comic) => {
+        {comics?.map((comic) => { {/*mapemento do response e exibição atraves de cards */}
           return (
 
-            <Card style={{ width: '18rem', margin: '20px', backgroundColor: 'red', color: 'white' }}>
+            <Card  style={{ width: '18rem', margin: '20px', backgroundColor: 'red', color: 'white' }}>
               <Card.Img variant="top" src={comic.thumbnail.path + '.' + comic.thumbnail.extension} />
               <Card.Body  >
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-
   test('must show input from search on the top of screen', () => {
     render(<App />);
     const inputSearch = screen.getByTestId('search')
@@ -10,12 +9,13 @@ import App from './App';
   })
 
 
-  test('must show cards on page ', () => {
-    render(<App />);
-    const cardElement = screen.getByTestId('cartao')
-    expect(cardElement).toBeInTheDocument();
-  })
+test ( 'must show cards with comics', () => {
 
+  render(<App />);
+  const elemet = screen.getByTestId('cartao')
+  expect(elemet).toBeInTheDocument();
+
+})
 
 
 
